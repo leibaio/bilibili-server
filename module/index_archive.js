276dc4227a63138ec38f1d1015b468cbf87ecda0
xlz122 <122874722@qq.com>
2024-07-08 15:48:24 +0800

    update: 更新首页分类、分类列表、排行榜分类接口

--- module/index_archive.js
+++ module/index_archive.js
@@ -1,9 +1,9 @@
-// 分类列表 - 最新视频
+// 首页分类 - 最新视频
 
 module.exports = (query, request) => {
   return request(
     'GET',
-    `https://api.bilibili.com/archive_rank/getarchiverankbypartion?tid=${query.tid}&pn=${query.page}`,
+    `https://api.bilibili.com/x/web-interface/dynamic/region?rid=${query.rid}&pn=${query.page}`,
     {},
     {
       crypto: 'm',
